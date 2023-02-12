374ae5c70304825c04f6a1732f806ada372020cd
Morten Westermann <s203965@student.dtu.dk>
2023-02-12 15:22:26 +0100

    Tensor Solve is somewhat functional again...

--- Optical_flow/tensor_solve.py
+++ Optical_flow/tensor_solve.py
@@ -1,43 +1,106 @@
 import numpy as np
 import time
 
-def tensor_solve(Vx,Vy,Vt, N = 3):
+def tensor_solve(Vx, Vy, Vt, N = 3):
     """
     Vx, Vy, and Vt must all be the same shape numpy array
     """
     # Assume N = 3
 
-    r = (N-1)//2; d = 2*r
     (n,m) = np.shape(Vx)
+    vector_field = np.zeros((2,n,m))
 
-    si = (n-d)*(m-d); sj = N**2  # Matrix dimensions
+    if (np.shape(Vy) != (n,m)) or (np.shape(Vt) != (n,m)): raise Exception("Gradients Must be the same shape!")
+    if N%2 == 0: raise Exception("N must be odd!")
+
+    # Somehow this method only works for square matrices
+    # Padding is therefore nessersary
+    k = max(n,m)
+    Vx = np.pad(Vx,((0,k-n),(0,k-m)), mode="constant")
+    Vy = np.pad(Vy,((0,k-n),(0,k-m)), mode="constant")
+    Vt = np.pad(Vt,((0,k-n),(0,k-m)), mode="constant")
+
+    r = (N-1)//2; d = 2*r
+    si = (k-d)**2; sj = N**2  # Matrix dimensions
 
     A0 = np.zeros((si,sj,2))
     b0 = np.zeros((si,sj,1))
-    vector_field = np.zeros((n,m,2))
 
-    if N%2 == 0: Warning("N must be odd!"); return np.transpose(vector_field,(2,1,0))
-
+    # grab all submatrices of size (k-d) by (k-d)
     for i in range(sj):
         x = i%N; y = i//N
         u = x-d; v = y-d
         if u == 0: u = None
         if v == 0: v = None
 
-        A0[:,i,0] = Vx[x:u, y:v].flatten()
-        A0[:,i,1] = Vy[x:u, y:v].flatten()
-        b0[:,i,0] = Vt[x:u, y:v].flatten()
+        A0[:,i,0] = Vx[y:v,x:u].T.flatten()
+        A0[:,i,1] = Vy[y:v,x:u].T.flatten()
+        b0[:,i,0] = Vt[y:v,x:u].T.flatten()
 
     AT = np.transpose(A0,(0,2,1))
-    A = np.matmul(AT,A0)
-    b = np.matmul(AT,b0)
+    A = np.matmul(AT, A0)
+    b = np.matmul(AT, b0)
 
+    # Make sure trivial zeros does not kill the solver :)
+    trivial_zeros = np.argwhere(np.all(A[..., :] == 0, axis=(1,2)))
+    A[trivial_zeros] = np.array([[1,1],[0,1]])
+    b[trivial_zeros] = np.array([[0],[1]])
+
+    # Magic!
     sol = np.linalg.solve(A,-b)
 
-    vector_field[r:-r,r:-r,:] = np.reshape(sol,(n-d,m-d,2))
-    vector_field = np.transpose(vector_field,(2,1,0))
-
+    # Reconstruct the vector field to the original size
+    vector_field[:,r:-r,r:-r] = np.reshape(sol,(k-d,k-d,2))[:n-d,:m-d].transpose((2,0,1))
+    
     return vector_field
+
+
+#-------------------------------------------------------------------
+
+# def square_tensor_solve(Vx, Vy, Vt, N = 3):
+#     """
+#     Vx, Vy, and Vt must all be the same shape square numpy array
+#     """
+#     # Assume N = 3
+
+#     (n,m) = np.shape(Vx)
+
+#     vector_field = np.zeros((2,n,m))
+
+#     if not n == m: raise Exception("Gradients must be square! - Use ordinary tensor solve otherwise")
+#     if (np.shape(Vy) != (n,m)) or (np.shape(Vt) != (n,m)): raise Exception("Gradients Must be the same shape!")
+#     if N%2 == 0: raise Exception("N must be odd!")
+
+#     # No padding nessersary :)
+#     k = n
+#     r = (N-1)//2; d = 2*r
+#     si = (k-d)**2; sj = N**2  # Matrix dimensions
+
+#     A0 = np.zeros((si,sj,2))
+#     b0 = np.zeros((si,sj,1))
+
+#     # grab all submatrices of size (k-d) by (k-d)
+#     for i in range(sj):
+#         x = i%N; y = i//N
+#         u = x-d; v = y-d
+#         if u == 0: u = None
+#         if v == 0: v = None
+
+#         A0[:,i,0] = Vx[y:v,x:u].T.flatten()
+#         A0[:,i,1] = Vy[y:v,x:u].T.flatten()
+#         b0[:,i,0] = Vt[y:v,x:u].T.flatten()
+
+#     AT = np.transpose(A0,(0,2,1))
+#     A = np.matmul(AT, A0)
+#     b = np.matmul(AT, b0)
+
+#     # Magic!
+#     sol = np.linalg.solve(A,-b)
+
+#     # Reconstruct the vector field to the original size
+#     vector_field[:,r:-r,r:-r] = np.reshape(sol,(k-d,k-d,2)).transpose((2,0,1))
+
+#     return vector_field
 
 
 #-------------------------------------------------------------------
@@ -47,19 +110,25 @@ if __name__ == "__main__":
 
     print("Testing Linalg Solve ..")
 
-    sample_size = (100,100)
-    result1 = np.zeros(1000); result2 = np.copy(result1)
+    sample_size = (240,320)
+    n_samples = 1000
+    N = 7
+
+    r = (N-1)//2
+    if n_samples >= 10:
+        n_percent = n_samples/100
+    else:
+        n_percent = n_samples
+
+    result1 = np.zeros(n_samples); result2 = np.copy(result1)
 
     Vx = np.random.rand(sample_size[0],sample_size[1])
     Vy = np.random.rand(sample_size[0],sample_size[1])
     Vt = np.random.rand(sample_size[0],sample_size[1])
 
-    N = 5
-    r = (N-1)//2
 
-
-    for i in range(1000):
-        if i%10 == 0: print(f"Completion: {i//10}%  ", end = "\r")
+    for i in range(n_samples):
+        if i%n_percent == 0: print(f"Completion: {i//n_percent}%  ", end = "\r")
         Vx[:,:] = np.random.rand(sample_size[0],sample_size[1])
         Vy[:,:] = np.random.rand(sample_size[0],sample_size[1])
         Vt[:,:] = np.random.rand(sample_size[0],sample_size[1])
@@ -70,18 +139,18 @@ if __name__ == "__main__":
 
     print("\nDone!\n")
 
-    print("Testing iterative lstsq loop ..")
-
     mu1 = np.mean(result1)
     sigma1 = np.std(result1)
+
+    print("Testing iterative lstsq loop ..")
 
     pos = np.mgrid[0:sample_size[0],0:sample_size[1]]
     vector_field = np.zeros((2,sample_size[0],sample_size[1]))
     x_list = pos[0].flatten()
     y_list = pos[1].flatten()
 
-    for i in range(1000):
-        if i%10 == 0: print(f"Completion: {i//10}%  ", end = "\r")
+    for i in range(n_samples):
+        if i%n_percent == 0: print(f"Completion: {i//n_percent}%  ", end = "\r")
         Vx[:,:] = np.random.rand(sample_size[0],sample_size[1])
         Vy[:,:] = np.random.rand(sample_size[0],sample_size[1])
         Vt[:,:] = np.random.rand(sample_size[0],sample_size[1])
@@ -118,18 +187,27 @@ if __name__ == "__main__":
     print("Comparing...")
 
     output1 = tensor_solve(Vx, Vy, Vt, N = N)
+
+    # print(np.shape(output1))
+    # print(np.shape(output2))
+
+    # print(output1[:,r:-r,r:-r])
+    # print(30*"-")
+    # print(output2[:,r:-r,r:-r])
+
     working_precision = 6
     n = 0
+    nn = 0
 
     for k in zip(output1[:,r:-r,r:-r].flatten(), output2[:,r:-r,r:-r].flatten()):
         if np.round(k[0], working_precision) != np.round(k[1], working_precision):
-            print(f"Error! {np.round(k[0], working_precision)} != {np.round(k[1], working_precision)}")
+            if k[1]: nn += 1; print(f"Error! {np.round(k[0], working_precision)} != {np.round(k[1], working_precision)}")
             n += 1
 
     if n == 0:
         print("Results compare OK!\n")
     else:
-        print(f"Found {n} errors!\n")
+        print(f"Found {n} errors of which {nn} is nontrivial!\n")
 
     print(f"Tensor Solve: Average is {np.round(mu1*1000,1)}ms and std is {np.round(sigma1*1000,1)}ms")
     print(f"Lstsq Loop: Average is {np.round(mu2*1000,1)}ms and std is {np.round(sigma2*1000,1)}ms\n")
