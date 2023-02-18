e6de9114b66eef6c53fc1ca56f0f1c6e17c8f1b2
Morten Westermann <s203965@student.dtu.dk>
2023-02-18 22:03:07 +0100

    Final Version!

--- Optical_flow/camera.py
+++ Optical_flow/camera.py
@@ -1,6 +1,7 @@
 import numpy as np
 import skimage
 import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 # import matplotlib.quiver
 from scipy import ndimage
 import cv2
@@ -9,13 +10,17 @@ from tensor_solve import *
 
 
 # Conditions
-N = 5   # Same N as other script...
-scale_factor = 1 # Scale factor for optical flow. Lower is better but slower
-figsize = (6,9)
-N_a = 40 # Distance between arrows
-sigma = 3
+N = 7   # Same N as other script...
+scale_factor = 4 # Scale factor for optical flow. Lower is better but slower
+figsize = (8,4.5)
+N_a = 8 # Distance between arrows
+sigma = 4
 
-image_source = "Optical_flow/Videos/Good_test_video.mp4"
+fps = 15
+name = "Spinning Wheel"
+
+
+image_source = "Optical_flow/Videos/Spinny_wheel.mp4"
 
 r = (N-1)//2
 
@@ -41,73 +46,48 @@ pos = np.mgrid[0:h:scale_factor,0:w:scale_factor]
 vector_field = np.zeros((2,h//scale_factor,w//scale_factor))
 
 
-opt_flow = plt.quiver(pos[1,::N_a,::N_a], pos[0,::N_a,::N_a], vector_field[0,::N_a,::N_a], vector_field[1,::N_a,::N_a], vector_field[0,::N_a,::N_a], cmap = "hot") #
+opt_flow = plt.quiver(pos[1,::N_a,::N_a], pos[0,::N_a,::N_a], vector_field[0,::N_a,::N_a], vector_field[1,::N_a,::N_a], vector_field[0,::N_a,::N_a], cmap = "hot", scale = 10000) #
 
 
-for i in range(N_im):
-    start = time.time()
-    ret, new_frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    
-    img = skimage.color.rgb2gray(new_frame)
-    downscaled_image_new = skimage.transform.downscale_local_mean(img,(scale_factor,scale_factor))
+movie_writer = anim.writers['ffmpeg']
+metadata = dict(title=name, authors = 'Aksel Buur Christensen, Karen Witness, Morten Westermann, Viktor Isaksen')
+movie = movie_writer(fps=fps, metadata=metadata)
 
 
-    image_stack = np.stack((downscaled_image_old, downscaled_image, downscaled_image_new))
-    # Shape: (3,y,x)
-    Vy = ndimage.sobel(image_stack, axis=1)[1,:,:]
-    Vx = ndimage.sobel(image_stack, axis=2)[1,:,:]
-    Vt = ndimage.sobel(image_stack, axis=0)[1,:,:]
-    
-    # Vy = ndimage.gaussian_filter1d(image_stack, sigma = sigma, order = 1, axis=1)[1,:,:]
-    # Vx = ndimage.gaussian_filter1d(image_stack, sigma = sigma, order = 1, axis=2)[1,:,:]
-    # Vt = ndimage.gaussian_filter1d(image_stack, sigma = sigma, order = 1, axis=0)[1,:,:]
+with movie.saving(fig, name + ".mp4", n_frames):
+    for i in range(N_im):
+        start = time.time()
+        ret, new_frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
+        
+        img = skimage.color.rgb2gray(new_frame)
+        downscaled_image_new = skimage.transform.downscale_local_mean(img,(scale_factor,scale_factor))
 
 
-    vector_field[:,:,:] = tensor_solve(Vx = Vx, Vy = Vy, Vt = Vt, N = N)
+        image_stack = np.stack((downscaled_image_old, downscaled_image, downscaled_image_new))
+        # Shape: (3,y,x)
+        Vy = ndimage.gaussian_filter1d(image_stack, axis=1, order = 1, sigma = sigma, mode="constant", cval=0)[1,:,:]
+        Vx = ndimage.gaussian_filter1d(image_stack, axis=2, order = 1, sigma = sigma, mode="constant", cval=0)[1,:,:]
+        Vt = ndimage.sobel(image_stack, axis=0)[1,:,:]
 
-    #print(tensor_solve(Vx = Vx.T, Vy = Vy.T, Vt = Vt.T, N = N))
-    # for j in range(np.size(x_list)):
-    #     # Try to implement np.tensordot
-    #     x0 = x_list[j]; y0 = y_list[j]
-    #     u1 = x0-r; u2 = x0+r+1; 
-    #     v1 = y0-r; v2 = y0+r+1
-    #     if u1 == 0: u1 = None
-    #     if v1 == 0: v1 = None
+        vector_field[:,:,:] = tensor_solve(Vx = Vx, Vy = Vy, Vt = Vt, N = N)
 
-    #     Vx_p = Vx[v1:v2, u1:u2].flatten()
-    #     Vy_p = Vy[v1:v2, u1:u2].flatten()
-    #     Vt_p = Vt[v1:v2, u1:u2].flatten()
+        # Lets remove small values
+        amplitude_field = np.sqrt(vector_field[0,::N_a,::N_a]**2 + vector_field[1,::N_a,::N_a]**2)
 
-    #     A = np.stack((Vx_p,Vy_p))
+        # Update Plot
+        background.set_data(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        opt_flow.set_UVC(vector_field[0,::N_a,::N_a], -vector_field[1,::N_a,::N_a], amplitude_field ) #
+        plt.draw()
+        movie.grab_frame()
+        
+        downscaled_image_old = np.copy(downscaled_image)
+        downscaled_image = np.copy(downscaled_image_new)
+        frame = np.copy(new_frame)
 
-    #     sol = np.linalg.lstsq(A.T, -Vt_p, rcond=None)
-    #     vector_field[0, x0, y0] = sol[0][0]
-    #     vector_field[1, x0, y0] = sol[0][1]
-
-    
-    # Lets remove small values
-    amplitude_field = np.sqrt(vector_field[0,::N_a,::N_a]**2 + vector_field[1,::N_a,::N_a]**2)
-    # neglect_value = 0.05
-    # vector_field[0,::N_a,::N_a][amplitude_field <= neglect_value] = 0
-    # vector_field[1,::N_a,::N_a][amplitude_field <= neglect_value] = 0
-
-    # Update Plot
-    background.set_data(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    opt_flow.set_UVC(vector_field[0,::N_a,::N_a], -vector_field[1,::N_a,::N_a], amplitude_field ) #
-    plt.pause(0.01)
-    
-    downscaled_image_old = np.copy(downscaled_image)
-    downscaled_image = np.copy(downscaled_image_new)
-    frame = np.copy(new_frame)
-
-
-
-    #plt.savefig(f'Optical_flow/VideoFlow/GV_flow_{i}.png', dpi = 70)
-
-    print(f"_Frametime: {int(np.ceil(1000*(time.time() - start)))}ms, max movement: {np.round(np.sqrt(np.max(amplitude_field)),2)}, zero-points: {(vector_field[:,:,:] == 0).sum()}                          ", end="\r")
+        print(f"_ Frame: {i}, Frametime: {int(np.ceil(1000*(time.time() - start)))}ms, max movement: {np.round(np.sqrt(np.max(amplitude_field)),2)}, zero-points: {(vector_field[:,:,:] == 0).sum()}                          ", end="\r")
 
 print("\nReleasing Camera")
 cam.release()
--- Optical_flow/gauss_camera.py
+++ Optical_flow/gauss_camera.py
@@ -1,6 +1,7 @@
 import numpy as np
 import skimage
 import matplotlib.pyplot as plt
+import matplotlib.animation as anim
 from scipy import ndimage
 import cv2
 import time
@@ -8,11 +9,15 @@ from tensor_solve import *
 
 
 # Conditions
-scale_factor = 2 # Scale factor for optical flow. Lower is better but slower
-figsize = (9,16)
-N_a = 20 # Distance between arrows
-sigma_1 = 4
+scale_factor = 6 # Scale factor for optical flow. Lower is better but slower
+figsize = (8,4.5)
+N_a = 8 # Distance between arrows
+sigma_1 = 2
 sigma_2 = 2
+sigma_3 = 1
+
+fps = 15
+name = "Rolling Bottle"
 
 image_source = "Optical_flow/Videos/Good_test_video.mp4"
 
@@ -31,21 +36,24 @@ vector_field = np.zeros((h//scale_factor,w//scale_factor,n_frames,2),dtype=np.fl
 
 for i in range(n_frames):
     ret, frame = cam.read()
+    if not ret:
+        print("Failed to grab frame!!")
+        quit()
     frame_stack[:,:,:,i] = np.int16(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    # image_stack[:,:,i] = skimage.color.rgb2gray(frame)
     image_stack_downscaled[:,:,i] = skimage.transform.downscale_local_mean(skimage.color.rgb2gray(frame),(scale_factor,scale_factor))
 
+cam.release()
 
-Vx = ndimage.gaussian_filter1d(image_stack_downscaled, sigma = sigma_1, order = 1, axis = 0)
-Vy = ndimage.gaussian_filter1d(image_stack_downscaled, sigma = sigma_1, order = 1, axis = 1)
-Vt = ndimage.gaussian_filter1d(image_stack_downscaled, sigma = sigma_1, order = 1, axis = 2)
+Vx = ndimage.gaussian_filter1d(image_stack_downscaled, sigma = sigma_1, order = 1, axis = 0, mode = 'nearest')
+Vy = ndimage.gaussian_filter1d(image_stack_downscaled, sigma = sigma_1, order = 1, axis = 1, mode = 'nearest')
+Vt = ndimage.gaussian_filter1d(image_stack_downscaled, sigma = sigma_1, order = 1, axis = 2, mode = 'nearest')
 
-sxx = ndimage.gaussian_filter(Vx*Vx, sigma = sigma_2)
-sxy = ndimage.gaussian_filter(Vx*Vy, sigma = sigma_2)
-syy = ndimage.gaussian_filter(Vy*Vy, sigma = sigma_2)
+sxx = ndimage.gaussian_filter(Vx*Vx, sigma = sigma_2, mode = 'nearest')
+sxy = ndimage.gaussian_filter(Vx*Vy, sigma = sigma_2, mode = 'nearest')
+syy = ndimage.gaussian_filter(Vy*Vy, sigma = sigma_2, mode = 'nearest')
 
-sxt = ndimage.gaussian_filter(Vx*Vt, sigma = sigma_2)
-syt = ndimage.gaussian_filter(Vy*Vt, sigma = sigma_2)
+sxt = ndimage.gaussian_filter(Vx*Vt, sigma = sigma_3, mode = 'nearest')
+syt = ndimage.gaussian_filter(Vy*Vt, sigma = sigma_3, mode = 'nearest')
 
 A = np.zeros((np.size(sxx), 2, 2))
 b = -np.zeros((np.size(sxx), 2))
@@ -60,11 +68,15 @@ b[:,1] = -syt.flatten()
 
 sol = np.linalg.solve(A,b)
 
-vector_field[:,:,:,:] = sol.reshape((h//scale_factor,w//scale_factor,n_frames,2))
+vector_field = sol.reshape((h//scale_factor,w//scale_factor,n_frames,2))
 
 fig, ax = plt.subplots(1, 1, figsize = figsize)
 background = plt.imshow(frame_stack[:,:,:,0])
 fig.suptitle("Camera")
+
+movie_writer = anim.writers['ffmpeg']
+metadata = dict(title=name, author = 'Aksel Buur Christensen, Karen Witness, Morten Westermann, Viktor Isaksen')
+movie = movie_writer(fps=fps, metadata=metadata)
 
 amplitude_field = vector_field[::N_a,::N_a,:,0]**2 + vector_field[::N_a,::N_a,:,1]**2
 
@@ -73,12 +85,14 @@ opt_flow = plt.quiver(pos[1,::N_a,::N_a], pos[0,::N_a,::N_a], vector_field[::N_a
 
 print(f"\nDone in {time.strftime('%-M minutes and %-S seconds', time.gmtime(time.time()-start))}")
 
-for i in range(n_frames):
-    # Update Plot
-    background.set_data(frame_stack[:,:,:,i])
-    opt_flow.set_UVC(vector_field[::N_a,::N_a,i,0], vector_field[::N_a,::N_a,i,1], amplitude_field[:,:,i])
-    plt.pause(0.1)
-    
+with movie.saving(fig, name + ".mp4", n_frames):
+    for i in range(n_frames):
+        #Update Plot
+        background.set_data(frame_stack[:,:,:,i])
+        opt_flow.set_UVC(vector_field[::N_a,::N_a,i,0], vector_field[::N_a,::N_a,i,1], amplitude_field[:,:,i])
+        if i == 200: print(np.max(amplitude_field[:,:,200]))
+        # write frame to video
+        plt.pause(0.1)
+        movie.grab_frame()
 
 print("\nReleasing Camera")
-cam.release()
