6cf06dda71297d292907d801210314b3fa18dfd2
Morten Westermann <s203965@student.dtu.dk>
2023-03-04 22:12:16 +0100

    Added spectral analysis

--- Salami/analysis2.py
+++ Salami/analysis2.py
@@ -3,72 +3,124 @@ import matplotlib.pyplot as plt
 import os 
 import helpFunctions as hf
 from compare_plot import *
+from spectral_analysis import *
 
 # for i in range(5):
-imName = "Salami/multispectral_day01.mat"
-annotationName = "Salami/annotation_day01.png"
-[multiIm, annotationIm] = hf.loadMulti(imName, annotationName)
+# imName = "Salami/multispectral_day01.mat"
+# annotationName = "Salami/annotation_day01.png"
+# [multiIm, annotationIm] = hf.loadMulti(imName, annotationName)
 
-fat_vector = multiIm[annotationIm[:,:,1], :]
-meat_vector = multiIm[annotationIm[:,:,2], :]
-
-
-print(np.shape(annotationIm[:,:, 0]))
-print(np.shape(multiIm[:,:]))
-print(np.shape(fat_vector))
-
-# amount of points in each annoted category for day 1
-m_fat = np.size(fat_vector[:,0])
-m_meat = np.size(meat_vector[:,0])
-
-# Means and standard devations for fat and meat categories
-fat_vector_means = np.mean(fat_vector, axis=0)
-meat_vector_means = np.mean(meat_vector, axis=0)
-fat_vector_sd = np.std(fat_vector, axis=0)
-meat_vector_sd = np.std(meat_vector, axis=0)
-
-# Finding the combined standard devation for fat and meat as
-combined_sd = 1/(m_fat + m_meat) * (m_fat*fat_vector_sd + m_meat*meat_vector_sd) # (strictly speaking useless)
-
-# finding the simple threshold under the assumption that the standard devs are the same corresponds
-# to finding the mid point between the 2 means
-t = (fat_vector_means + meat_vector_means)/2
-
-# Finding the error classification rates
-error_rate = ( np.sum(fat_vector < t, axis=0) + np.sum(meat_vector > t, axis=0)  ) / (m_fat + m_meat)
-print(np.round(error_rate*100,2))
-
-# The best spectral band for this simple analysis is
-best_band = np.argmin(error_rate) # This is 0-indexed
-print(f"the best band is { best_band+1}")
-
-# the background index is the not index of all the annotations added together
-index_background = (annotationIm[:,:,0]+annotationIm[:,:,1]+annotationIm[:,:,2])==0
-
-# Classifying the entire image with band 14 gives us
-entire_im = np.copy(multiIm[:,:,best_band])
-index_fat = (entire_im > t[best_band])
-index_meat = (entire_im < t[best_band])
-entire_im[index_fat] = 1 #set all fat values to class 1
-entire_im[index_meat] = 2 # set all meat values to class 2
-entire_im[index_background] = 0 # set background to class 0
-
-compare_image(image = entire_im, day = 1 , title = "Day 1")
-
-#plt.imshow(entire_im)
+# fat_vector = multiIm[annotationIm[:,:,1], :]
+# meat_vector = multiIm[annotationIm[:,:,2], :]
 
 
-"""
-Figures 
-"""
+# print(np.shape(annotationIm[:,:, 0]))
+# print(np.shape(multiIm[:,:]))
+# print(np.shape(fat_vector))
 
-# plt.figure()
-# for j in range(19):
-#     plt.clf()
-#     fat_hist = plt.hist(fat_vector[:, j], range=[0, 100], bins=50, color='orange')
-#     meat_hist = plt.hist(meat_vector[:, j], range=[0,100], bins=50, color='blue')
-#     plt.title(f"band {j}")
-#     # plt.legend('fat', 'meat')
-#     plt.pause(1/2)
+# # amount of points in each annoted category for day 1
+# m_fat = np.size(fat_vector[:,0])
+# m_meat = np.size(meat_vector[:,0])
 
-compare_spectrum(fat_vector_means, fat_vector_sd, meat_vector_means, meat_vector_sd, title = "Mean Intensity of Sausage at Day 1")
+# # Means and standard devations for fat and meat categories
+# fat_vector_means = np.mean(fat_vector, axis=0)
+# meat_vector_means = np.mean(meat_vector, axis=0)
+# fat_vector_sd = np.std(fat_vector, axis=0)
+# meat_vector_sd = np.std(meat_vector, axis=0)
+
+# # Finding the combined standard devation for fat and meat as
+# combined_sd = 1/(m_fat + m_meat) * (m_fat*fat_vector_sd + m_meat*meat_vector_sd) # (strictly speaking useless)
+
+# # finding the simple threshold under the assumption that the standard devs are the same corresponds
+# # to finding the mid point between the 2 means
+# t = (fat_vector_means + meat_vector_means)/2
+
+# # Finding the error classification rates
+# error_rate = ( np.sum(fat_vector < t, axis=0) + np.sum(meat_vector > t, axis=0)  ) / (m_fat + m_meat)
+# print(np.round(error_rate*100,2))
+
+# # The best spectral band for this simple analysis is
+# best_band = np.argmin(error_rate) # This is 0-indexed
+# print(f"the best band is { best_band+1}")
+
+# # the background index is the not index of all the annotations added together
+# index_background = (annotationIm[:,:,0]+annotationIm[:,:,1]+annotationIm[:,:,2])==0
+
+# # Classifying the entire image with band 14 gives us
+# entire_im = np.copy(multiIm[:,:,best_band])
+# index_fat = (entire_im > t[best_band])
+# index_meat = (entire_im < t[best_band])
+# entire_im[index_fat] = 1 #set all fat values to class 1
+# entire_im[index_meat] = 2 # set all meat values to class 2
+# entire_im[index_background] = 0 # set background to class 0
+
+# #compare_image(image = entire_im, day = 1 , title = "Day 1 - True Classification")
+
+# #plt.imshow(entire_im)
+
+
+# """
+# Figures 
+# """
+
+# # plt.figure()
+# # for j in range(19):
+# #     plt.clf()
+# #     fat_hist = plt.hist(fat_vector[:, j], range=[0, 100], bins=50, color='orange')
+# #     meat_hist = plt.hist(meat_vector[:, j], range=[0,100], bins=50, color='blue')
+# #     plt.title(f"band {j}")
+# #     # plt.legend('fat', 'meat')
+# #     plt.pause(1/2)
+
+# #compare_spectrum(fat_vector_means, fat_vector_sd, meat_vector_means, meat_vector_sd, title = "Mean Intensity of Sausage at Day 1")
+
+
+
+# ##  Discriminant method
+# Sigma_fat = np.cov(fat_vector, rowvar = False)
+# Sigma_meat = np.cov(meat_vector, rowvar = False)
+
+# m_fat = np.size(fat_vector,0) - 1
+# m_meat = np.size(meat_vector,0) - 1
+
+# Sigma = 1/(m_fat + m_meat) * (m_fat*Sigma_fat + m_meat * Sigma_meat)
+
+# Sigma_inv = np.linalg.inv(Sigma)
+
+# mu_fat = np.copy(fat_vector_means)
+# mu_meat = meat_vector_means
+
+# XT = np.reshape(multiIm,(514*514,19))
+
+# S_fat = (np.linalg.multi_dot([XT, Sigma_inv, mu_fat]) - 1/2 * np.linalg.multi_dot([mu_fat.T, Sigma_inv, mu_fat])).reshape((514,514))
+# S_meat = (np.linalg.multi_dot([XT, Sigma_inv, mu_meat]) - 1/2 * np.linalg.multi_dot([mu_meat.T, Sigma_inv, mu_meat])).reshape((514,514))
+
+# # Post processing
+
+# true_fat_index = annotationIm[:,:,1]
+# true_meat_index = annotationIm[:,:,2]
+
+# index_fat2 = (S_fat > S_meat)
+# index_meat2 = (S_meat >= S_fat)
+# entire_im2 = construct_entire_im(np.shape(multiIm[:,:,0]),index_fat2,index_meat2,index_background)
+
+# #Chekc the result
+# compute_errorrate(true_fat_index, true_meat_index, index_fat2, index_meat2)
+# compare_image(image = entire_im2, day = 1 , title = "Day 1 - Classification by means of all spectral bands")
+
+
+
+multiIm, true_fat, true_meat, index_background, fat_vector, meat_vector = load_day_data(1)
+
+# Threshold Value
+t, best_band = train_threshold_value(fat_vector, meat_vector)
+index_fat, index_meat = compute_threshold_value(multiIm,t, best_band)
+errorrate_1 = compute_errorrate(true_fat, true_meat, index_fat, index_meat, method_name = "TV")
+
+# Multivariate Linear Discriminant
+Sigma_inv, mu_fat, mu_meat, k = train_multivariate_linear_discriminant(multiIm, fat_vector, meat_vector)
+index_fat, index_meat = compute_multivariate_linear_discriminant(multiIm, Sigma_inv, mu_fat, mu_meat, k)
+errorrate_2 = compute_errorrate(true_fat, true_meat, index_fat, index_meat, method_name = "MLD")
+
+#Evaluation
+print("The best method is " + "TV"*int(errorrate_1 < errorrate_2) + "MLD"*int(errorrate_1 >= errorrate_2))
---
+++ Salami/spectral_analysis.py
@@ -0,0 +1,111 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
+import helpFunctions as hf
+
+
+
+
+
+
+
+def load_day_data(day):
+    str_day = "0"*(day<10)+str(day)
+    imName = "Salami/multispectral_day" + str_day + ".mat"
+    annotationName = "Salami/annotation_day" + str_day + ".png"
+    [multiIm, annotationIm] = hf.loadMulti(imName, annotationName)
+    
+    true_fat = annotationIm[:,:,1]
+    true_meat = annotationIm[:,:,2]
+    index_background = (annotationIm[:,:,0] + annotationIm[:,:,1] + annotationIm[:,:,2]) == 0
+
+    fat_vector = multiIm[true_fat, :]
+    meat_vector = multiIm[true_meat, :]
+
+    return multiIm, true_fat, true_meat, index_background, fat_vector, meat_vector
+
+# ---------------------------------------------------------------------
+
+def compute_errorrate(true_fat, true_meat, index_fat, index_meat, method_name = None):
+    correct_fat = np.sum(index_fat[true_fat] == 1)/np.sum(true_fat)
+    correct_meat = np.sum(index_meat[true_meat] == 1)/np.sum(true_meat)
+
+    errorrate = (np.sum(index_fat[true_fat] == 0) + np.sum(index_meat[true_meat] == 0)) / (np.sum(true_fat) + np.sum(true_meat))
+
+    if method_name != None: print(f"{method_name} guessed correctly {correct_fat*100:.2f}% for fat and {correct_meat*100:.2f}% for meat")
+    return errorrate
+
+# ---------------------------------------------------------------------
+
+def construct_entire_im(index_fat, index_meat, index_background):
+    entire_im = np.zeros(np.shape(index_fat))
+    entire_im[index_fat] = 1
+    entire_im[index_meat] = 2
+    entire_im[index_background] = 0
+    return entire_im
+# ---------------------------------------------------------------------
+
+def train_multivariate_linear_discriminant(multiIm, fat_vector, meat_vector):
+
+    # Preprocessing
+    m_fat = np.size(fat_vector,0) - 1
+    m_meat = np.size(meat_vector,0) - 1
+
+    mean_fat = np.mean(fat_vector,axis=0)
+    mean_meat = np.mean(meat_vector,axis=0)
+
+    k = [np.size(multiIm,0), np.size(multiIm,1)]
+
+    # Processing
+    Sigma_fat = np.cov(fat_vector, rowvar = False)
+    Sigma_meat = np.cov(meat_vector, rowvar = False)
+
+    Sigma = 1/(m_fat + m_meat) * (m_fat*Sigma_fat + m_meat * Sigma_meat)
+
+    Sigma_inv = np.linalg.inv(Sigma)
+
+    mu_fat = np.copy(mean_fat)
+    mu_meat = mean_meat
+
+    return Sigma_inv, mu_fat, mu_meat, k
+
+# ---------------------------------------------------------------------
+
+def compute_multivariate_linear_discriminant(multiIm, Sigma_inv, mu_fat, mu_meat, k, pi = 1):
+
+    XT = XT = np.reshape(multiIm,(k[0]*k[1],np.size(mu_fat)))
+
+    S_fat = (np.linalg.multi_dot([XT, Sigma_inv, mu_fat]) - 1/2 * np.linalg.multi_dot([mu_fat.T, Sigma_inv, mu_fat]) + np.log(pi)).reshape(k)
+    S_meat = (np.linalg.multi_dot([XT, Sigma_inv, mu_meat]) - 1/2 * np.linalg.multi_dot([mu_meat.T, Sigma_inv, mu_meat]) + np.log(pi)).reshape(k)
+    
+    index_fat = (S_fat > S_meat)
+    index_meat = (S_fat <= S_meat)
+
+    return index_fat, index_meat
+
+# ---------------------------------------------------------------------
+
+def train_threshold_value(fat_vector, meat_vector):
+
+    # Preprocessing
+    m_fat = np.size(fat_vector,0)
+    m_meat = np.size(meat_vector,0)
+    mean_fat = np.mean(fat_vector,axis=0)
+    mean_meat = np.mean(meat_vector,axis=0)
+
+    t = (mean_fat + mean_meat)/2
+
+    error_rate = ( np.sum(fat_vector < t, axis=0) + np.sum(meat_vector > t, axis=0)  ) / (m_fat + m_meat)
+    best_band = np.argmin(error_rate)
+
+    return t, best_band
+
+# ---------------------------------------------------------------------
+
+def compute_threshold_value(multiIm, t, best_band):
+    best_band_im = multiIm[:,:,best_band]
+    index_fat = (best_band_im > t[best_band])
+    index_meat = (best_band_im <= t[best_band])
+
+    return index_fat, index_meat
+    
