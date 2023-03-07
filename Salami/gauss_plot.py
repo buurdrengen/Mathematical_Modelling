7f4238bea784aac351bd47734dc5a1f37fd6c188
Morten Westermann <s203965@student.dtu.dk>
2023-03-07 11:09:40 +0100

    Changed Compare_plot

--- Salami/gauss_plot.py
+++ Salami/gauss_plot.py
@@ -51,10 +51,11 @@ print(f"Critical point: {(mu1+mu2)/2}")
 
 
 # Opgave 1.5)
-ax = plt.figure().add_subplot(projection='3d')
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
 
 # Make data.
-x = np.arange(-5,5,0.05)
+x = np.arange(-5,5,0.025)
 x1, x2 = np.meshgrid(x, x)
 
 mu1 = 0; mu2 = 1
@@ -63,16 +64,15 @@ var1 = sigma1**2; var2 = sigma2**2
 
 g = 1/(2*np.pi) * 1/(sigma1*sigma2) * np.exp(-1/2 * ( 1/var1*np.power(x1 - mu1,2) + 1/var2*np.power(x2 - mu2,2)))
 
-ax.plot_surface(x1,x2,g, cmap = "gist_ncar", linewidth=0)
+surf = ax.plot_surface(x1,x2,g, cmap = "gist_ncar", linewidth=0, rstride=1, cstride=1)
 plt.show()
-
 
 # Opgave 1.6)
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 plt.rcParams['text.usetex'] = True
 
-x = np.arange(-6,6,0.05)
+x = np.arange(-6,6,0.025)
 x1, x2 = np.meshgrid(x, x)
 # Make data.
 covar = sigma1*sigma2; rho = 2/3
@@ -80,17 +80,18 @@ covar = sigma1*sigma2; rho = 2/3
 
 def g_cor(rho):
     return 1/(2*np.pi) * 1/(sigma1*sigma2) * 1/np.sqrt(1 - rho**2) * np.exp(-1/2 * 1/(1 - rho**2) * ( 1/var1*np.power(x1 - mu1,2) - 2*rho/covar*(x1 - mu1)*(x2 - mu2) + 1/var2*np.power(x2 - mu2,2)))
-g_cors = g_cor(0)
+g_cors = g_cor(rho=rho)
 
-surf = ax.plot_surface(x1,x2,g_cors, cmap = "gist_ncar", linewidth=0)
+surf = ax.plot_surface(x1,x2,g_cors, cmap = "gist_ncar", linewidth=0, antialiased=False, rstride=1, cstride=1)
 ax.zaxis.set_major_locator(LinearLocator(6))
 ax.set_xlabel(r'$x_1$')
 ax.set_ylabel(r'$x_2$')
 ax.set_zlabel(r'$P(x_1,x_2)$')
 
 ax.set_zlim([0,0.03])
+fig.colorbar(surf, shrink=0.7, aspect=20, pad=0.10, ticks = np.arange(0,0.035,0.005))
 
-# Make an animation
+#Make an animation
 name = "Covariant Gaussian Distribution"
 fps = 24
 movie_writer = anim.writers['ffmpeg']
@@ -99,14 +100,19 @@ movie = movie_writer(fps=fps, metadata=metadata)
 
 with movie.saving(fig, name + ".mp4", 100):
     for i in range(100):
+        print("_Frame: " + str(i), end = "\r")
         fig.suptitle(r'$\rho$ = ' + f'{0.01*i:0.2f}', fontsize=16)
         new_data = g_cor(0.01*i)
         surf.remove()
-        surf = ax.plot_surface(x1,x2,new_data, cmap = "gist_ncar", linewidth=0)
+        surf = ax.plot_surface(x1,x2,new_data, cmap = "gist_ncar", linewidth=0, antialiased=False, rstride=1, cstride=1)
         plt.draw()
         movie.grab_frame()
 
-new_data = g_cor(2/3)
+new_data = g_cor(rho=rho)
 surf.remove()
-surf = ax.plot_surface(x1,x2,new_data, cmap = "gist_ncar", linewidth=0)
+surf = ax.plot_surface(x1,x2,new_data, cmap = "gist_ncar", linewidth=0, antialiased=False, rstride=1, cstride=1)
+fig.suptitle(r'$\rho = \frac{2}{3}$', fontsize=16)
+plt.draw()
+plt.savefig("Salami/Gauss.png", dpi = 300)
 plt.show()
+print("\nDone      ")
