d2e8f44f7187fab3a04416682d7df9a0f03040bc
Viktor <isaksenviktor@gmail.com>
2023-04-13 08:00:23 +0200

    first day

--- Exam_02526/viktor_edits.py
+++ Exam_02526/viktor_edits.py
@@ -1,23 +1,40 @@
 import numpy as np
 import scipy.io
+import matplotlib.pyplot as plt
 import paralleltomo # [A,theta,p,d] = paralleltomo(N,theta,p,d)
 
 
 # Load the image with lead and steel shot
 data = np.array(scipy.io.loadmat("Exam_02526/testImage.mat")['im']) #Pixel size: 0.1 mm
 downsized_im = data[::100, ::100] # pixel size: 10 mm
+x = np.copy(downsized_im.flatten())
 
-
-# Initialize settings for paralleltomo
+# Initialize system matrix and stuff
 N = downsized_im.shape[0]
 theta = np.array([np.arange(0, 180, 1)])
-theta =np.matrix(np.linspace(0,179,179))
-print(theta)
-quit()
-[A,theta,p,d] = paralleltomo.paralleltomo(N, theta, 50) 
+p = 50
+d = 0.5 # [m]
+[A,theta,p,d] = paralleltomo.paralleltomo(N, theta, p)
 
-# Load system matrix A
-paralleltomo
+
+# Load simulated forward projection 
+b = A @ x # Finding b
+b_perturbed = b + np.random.normal(0, 0.1, size=np.shape(b)) # adding noise
+
+print(np.shape(A))
+print(np.shape(x))
+print(np.shape(b))
+
+x_new = np.linalg.solve(A.T @ A, A.T @ b_perturbed)
+
+print(np.sum(x_new-x)) 
+
+
+# plt.hist(b)
+# plt.imshow(data)
+plt.show()
+
+
 
 
 
