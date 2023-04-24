77eaca8b5f41ee3964c2f94b82e31fc56b9fa601
Morten Westermann <s203965@student.dtu.dk>
2023-04-24 20:33:19 +0200

    Extended variable exploration

--- Exam_02526/variable_exploration.py
+++ Exam_02526/variable_exploration.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
 import time
-import paralleltomo # [A,theta,p,d] = paralleltomo(N,theta,p,d)
+from paralleltomo import * # [A,theta,p,d] = paralleltomo(N,theta,p,d)
 
 # Load a seed for rng so that answers are reproducible and a time to test script
 np.random.seed(1)
@@ -12,12 +12,13 @@ start = time.time()
 data = np.array(scipy.io.loadmat("Exam_02526/testImage.mat")['im']) #Pixel size: 0.1 mm, 5000x5000 pixels
 
 # Load a resolution list
-res_list = np.array([100,200])
+res_list = np.array([200])
 N_outer = np.size(res_list)
 
 # Create a meshgrid for the internal colormaps
-theta_res_list = np.array([1, 2, 3])
-p_list = np.array([50, 60])#, 70, 80, 90, 100])
+resd = np.arange(1,50)
+theta_res_list = resd # np.array([1, 2, 3, 4, 5, 6])
+p_list = (resd + 1)*3 # np.array([30,40,50, 60, 70, 80, 90, 100])
 meshgrid_inner = np.meshgrid(theta_res_list, p_list)
 theta_mesh = meshgrid_inner[0].flatten()
 p_mesh = meshgrid_inner[1].flatten()
@@ -42,11 +43,11 @@ for i in range(N_outer):
     for j in range(N_inner):
         # Initialize inner loops parameters
         theta_res = theta_mesh[j]
-        theta = np.array([np.arange(0, 180, theta_res)])
+        theta = np.linspace(0,180,max(theta_res)) #np.array([np.arange(0, 180, theta_res)])
         p = p_mesh[j]
 
         # Initialize system matrix
-        [A,_,_,_] = paralleltomo.paralleltomo(N, theta, p)
+        [A,_,_,_] = paralleltomo(N, theta, p)
         sensitivity = np.linalg.cond(A)
         print(f"The condition number of our matrix A is given as {sensitivity}. Time passed {np.round(time.time()- start, 2)} seconds")
 
@@ -55,23 +56,27 @@ for i in range(N_outer):
 # Reshape the matrix so that the dimensions of the matrix correspond to the dimensions of the variables
 matrix_cond = np.reshape(matrix_cond, (np.size(res_list), np.size(p_list), np.size(theta_res_list)))
 
-fig, ax = plt.subplots(ncols=res_list.size, sharex=True, layout='constrained')
+fig, ax = plt.subplots(figsize = [4*res_list.size,4], ncols=res_list.size, sharex=True, layout='constrained')
+tick_scale_factor = 4
 
 for i in range(res_list.size):
-    #Create image for each resolution 
-    im=ax[i].imshow(matrix_cond[i,:,:], vmin=0, vmax=1e3)
+    #Create image for each resolution
+    if res_list.size == 1 : ax = [ax] 
+    im=ax[i].imshow(matrix_cond[i,:,:], vmin=0, vmax=1e3, cmap="ocean")
     
     # Define x- and yticks and set the resolution label
-    ax[i].set_xticks(range(theta_res_list.size), (180/theta_res_list).astype(int))
+    ax[i].set_xticks(range(theta_res_list.size)[::tick_scale_factor], (180/theta_res_list).astype(int)[::tick_scale_factor])
     ax[i].set_title(f"Res: ({int(5000/res_list[i])} x {int(5000/res_list[i])})")
-    ax[i].set_yticks(range(p_list.size), p_list)
+    ax[i].set_yticks(range(p_list.size)[::tick_scale_factor], p_list[::tick_scale_factor])
     ax[i].set_xlabel('# of angles')
-fig.suptitle('Sensitivity Stude')# Set title
+fig.suptitle('Sensitivity Study')# Set title
 ax[0].set_ylabel(f"No. of Rays") # Create the resolution label
 
 
 # Colorbar. This can be shared because an upper and lower limit on the image is defined.
 fig.colorbar(im, ax=ax)
+plt.draw()
+plt.savefig("N25-plot",dpi=300,format="png")
 plt.show()
 
 
