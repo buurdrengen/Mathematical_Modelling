1bfbf817e4e0b81e358aaf70c1e677e22b6e636d
Morten Westermann <s203965@student.dtu.dk>
2023-05-05 13:29:14 +0200

    fix

--- Exam_02526/viktor_edits.py
+++ Exam_02526/viktor_edits.py
@@ -64,6 +64,6 @@ for tree_type in tree_types:
                     ring_count = tree_ring_no,
                     vol_pellet = vol_pellet
                     )
-        print(f"Ended simulation in time: {start - time.time():0.1} seconds")
+        print(f"Ended simulation in time: {time.strftime('%H:%M:%S', time.gmtime(time.time()-start))}")
         plt.close('all')
 
